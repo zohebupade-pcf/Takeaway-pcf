24a06423bcae89ba650a09831211d26622d00e78
zohebupade-pcf <55667690+zohebupade-pcf@users.noreply.github.com>
2019-09-22 16:22:01 -0400

    Add files via upload

---
+++ Takeaway-pcf/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Zoheb Upade on 9/22/19.
+//  Copyright © 2019 Zoheb Upade. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
